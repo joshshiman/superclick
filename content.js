467d93aa5ca2f2abedf6f0eb235fe075ddc1665b
Joshua Shiman <joshshiman@proton.me>
2025-03-12 17:41:20 -0400

    Remove HOST permissions

--- content.js
+++ content.js
@@ -1,14 +1,12 @@
 document.addEventListener('mousedown', function(event) {
-    // Check for Command (metaKey) + Shift + Left Click (button 0)
-    if (event.metaKey && event.shiftKey && event.button === 0) {
-      // Ensure the click is not on a link to avoid conflicts
-      if (event.target.tagName !== 'A') {
-        const selectedText = window.getSelection().toString().trim();
-        if (selectedText) {
-          // Send a message to the background script with the selected text
-          chrome.runtime.sendMessage({ action: "search", text: selectedText });
-          event.preventDefault(); // Optional: Prevents default actions
-        }
+  if (event.metaKey && event.shiftKey && event.button === 0) {
+    if (event.target.tagName !== 'A') {
+      const selectedText = window.getSelection().toString().trim();
+      if (selectedText) {
+        const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(selectedText)}`;
+        window.open(searchUrl, '_blank'); 
+        event.preventDefault(); 
       }
     }
-  });
+  }
+});
